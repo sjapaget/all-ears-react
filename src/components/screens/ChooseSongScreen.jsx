5709e084f8599e041b8556808b68f5c2830bd6a7
Theodore Saulnier <theodore1712@yahoo.fr>
2024-02-08 11:53:00 +0100

    Added inputs for song data

--- src/components/screens/ChooseSongScreen.jsx
+++ src/components/screens/ChooseSongScreen.jsx
@@ -8,10 +8,37 @@ export default function ChooseSongScreen(props) {
 
   const [currentUser, setCurrentUser] = useState(0);
 
+  const fetchSpotifySong = (e) => {
+    e.preventDefault();
+    console.log("In fetchSpotifySong");
+  }
+
   return (
     <>
       <h1>Round #{roundNumber}</h1>
       <h2>{userNicknames[currentUser]}, pick a song !</h2>
+
+      <form action="" method="get" onSubmit={fetchSpotifySong}>
+        <label htmlFor="song-title">Title</label>
+        <input
+          type="text"
+          id="song-title"
+          name="song-title"
+        />
+        <label htmlFor="artist">Artist</label>
+        <input
+          type="text"
+          id="artist"
+          name="artist"
+        />
+        <label htmlFor="album">Album</label>
+        <input
+          type="text"
+          id="album"
+          name="album"
+        />
+        <button type="submit">Search on Spotify</button>
+      </form>
     </>
   )
 }
