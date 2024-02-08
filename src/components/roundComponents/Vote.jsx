1930f1e48e9259fa7e442794d5198f58f1abb3c5
Samuel Paget <sjapaget@gmail.com>
2024-02-08 13:34:01 +0100

    add Vote component with pseudocode

---
+++ src/components/roundComponents/Vote.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function Vote(props) {
+    const {
+        
+    } = props;
+
+    return (
+        <>
+            {/* 
+                1. Render name of player whose turn it is to vote w/ button to start vote
+                2. Player clicks start
+                3. Render a grid of buttons with players nicknames.
+                4. Player clicks who they're voting for
+
+                If still players who need to vote, GOTO 1
+
+                5. Store result of vote to be revealed later & restart roundsteps from
+                PlayRandomSong component.
+            */}
+        </>
+    )
+  }
