fb837fe972a815aa3d9ac9db1fc5349b7294ba0f
Theodore Saulnier <theodore1712@yahoo.fr>
2024-02-14 11:37:01 +0100

    Fix - resetting userIndex when all players have voted
    Sidenote: removed comments

--- src/components/roundComponents/Vote.jsx
+++ src/components/roundComponents/Vote.jsx
@@ -19,14 +19,6 @@ export default function Vote(props) {
       countDown == 0 && playerVotes(-1)
     }, [countDown]);
 
-    function Buttons({ number }) {
-      const buttons = [];
-      for(let i = 0; i < number; i++) {
-        buttons.push(<button onClick={() => playerVotes(i)} key={i}>{userNicknames[i]}</button>);
-      }
-      return buttons;
-    }
-
     function playerVotes(index) {
       const playerVotedFor = index >= 0 ? userNicknames[index] : null
       setVotes([
@@ -42,25 +34,23 @@ export default function Vote(props) {
     const updateUserIndex = () => {
       if(userIndex == userNicknames.length - 1) {
         setRoundStep(2);
+        setUserIndex(0);
       } else {
         setUserIndex(userIndex + 1);
         setCountDown(10);
       }
     }
 
+    function Buttons({ number }) {
+      const buttons = [];
+      for(let i = 0; i < number; i++) {
+        buttons.push(<button onClick={() => playerVotes(i)} key={i}>{userNicknames[i]}</button>);
+      }
+      return buttons;
+    }
+
     return (
         <>
-            {/*
-                1. Render name of player whose turn it is to vote w/ button to start vote
-                2. Player clicks start
-                3. Render a grid of buttons with players nicknames.
-                4. Player clicks who they're voting for
-
-                If still players who need to vote, GOTO 1
-
-                5. Store result of vote to be revealed later & restart roundsteps from
-                PlayRandomSong component.
-            */}
           <h2>{userNicknames[userIndex]}, your time to vote</h2>
           <p>{countDown}</p>
           <Buttons number={userNicknames.length}/>
